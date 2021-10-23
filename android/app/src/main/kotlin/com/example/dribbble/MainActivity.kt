e09ff21b5517730946f8951fb4a25d2f879ff680
Angga Nur Prasetya <angganurprasetya5@gmail.com>
2021-10-23 10:21:25 +0700

    Add APK

--- android/app/src/main/kotlin/com/example/dribbble/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.dribbble
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/minerva/dribbble/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.minerva.dribbble
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
